a39155b96fcdf7c3d5048db703ceb049222c9a55
thienpham8 <thienpham8@gmail.com>
2017-02-09 10:57:16 -0800

    Add files via upload

---
+++ user.py
@@ -0,0 +1,27 @@
+#K.Koltermann
+import os
+import sys
+import const #should have a constant file
+
+#should not store password in user object
+class user(Object):
+    userCount = 0
+    def __init__(self,username):
+        self.username = username
+        self.zipcode = "00000" #no zipcode
+        self.email = "NONE"
+        self.favorites = []
+        self.phoneNumber = "NONE"
+
+    def addFavorite(self,link):
+        self.favorites.append(link)
+
+    def resetDefault(self):
+        self.zipcode = "00000" #no zipcode
+        self.email = "NONE"
+        self.favorites = []
+        self.phoneNumber = "NONE"
+
+#TODO: ADD MORE SHIT
+        
+        
