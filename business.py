86b964cf48828c40f88564ec09aa0968b9eca3ae
thienpham8 <thienpham8@gmail.com>
2017-02-09 13:11:46 -0800

    Add files via upload

---
+++ business.py
@@ -0,0 +1,47 @@
+#K.Koltermann
+import os
+import sys
+import const
+
+class business(Object):
+    def __init__(self,businessName,ID,address,zipcode):
+        self.businessName = businessName
+        #ID = "0123456789 etc"
+        self.ID = ID
+        #Address = "STRING"
+        self.address = address
+        self.zipcode = zipcode
+        self.userClaimed = ""
+        self.claimed = False
+        self.locked = False
+        self.rating = const.RATING
+        self.cost = const.COST
+
+    def getInfo(self):
+        a = [self.businessName,self.ID,self.address,self.zipcode,self.rating,self.cost]
+        return a
+
+    def allowClaimed(self,username):
+        self.userClaimed.append(username)
+        self.claimed = False
+
+    def denyClaimed(self,username):
+        if username in self.userClaimed():
+            self.userClaimed.remove(username)
+        #failsafe
+        else:
+            self.userClaimed = []
+            self.claimed = False
+        if not self.userClaimed:
+            self.claimed = False
+
+    def unlock(self):
+        self.locked = False
+
+    def lock(self):
+        self.locked = True
+
+
+
+        
+        
