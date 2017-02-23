6d355c9e021d0ef41b38b1f9935b6ba88e7ad259
thienpham8 <thienpham8@gmail.com>
2017-02-22 22:26:12 -0800

    Add files via upload

---
+++ review.py
@@ -0,0 +1,21 @@
+#T.Pham
+import os
+import sys
+import const
+import dbConector
+
+#working
+class Review(object):
+	
+	
+	def __init__(self, reviewID="", userID="", userRating=0, userThumbs=True):
+		
+		self.reviewID = reviewID
+		self.userID = userID
+		self.userRating = rating
+		self.userThumbs = thumbs
+		
+		
+		def getReview(self):
+		review = [self.reviewID,self.userID
+		
